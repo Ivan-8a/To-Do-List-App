4517d05b3bf41f89fdf49edc10669eec616c630b
ivan8a <isantoyo0@ucol.mx>
2024-05-17 05:29:18 -0600

    prueba

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,5 @@
 const staticCacheName = 'site-static';
 
-// En el archivo service-worker.js
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/style.css',
-        '/script.js'
-      ]);
-    })
-  );
-});
-
-// En el archivo service-worker.js
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== 'v1') {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// En el archivo service-worker.js
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open('v1').then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request);
-      });
-    })
-  );
-});
-
-// En el archivo service-worker.js
-self.addEventListener('updatefound', event => {
-  console.log('New service worker found:', event);
-});
-
-self.addEventListener('controllerchange', event => {
-  console.log('Service worker updated:', event);
-});
 
 const assets = [
   '/',
