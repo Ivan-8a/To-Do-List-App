6089955b42e986a9616fa30cd8a056da644a5e00
ivan8a <isantoyo0@ucol.mx>
2024-05-21 12:32:41 -0600

    Avance, Funcionamiento offline

--- script.js
+++ script.js
@@ -113,3 +113,14 @@ document.addEventListener('DOMContentLoaded', function() {
     loadTasks();
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed: ', error);
+      });
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,32 @@
 const staticCacheName = 'site-static';
-
-
 const assets = [
   '/',
   '/index.html',
   '/script.js',
-  '/style.css',
-  '/lista.png'
+  '/lista.png',
+  '/style.css'
 ]
 
+// Install Service Worker
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then(cache => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  )
-})
-
-self.addEventListener('activate', evt => {
-
+  console.log('Service Worker has been installed')
+  evt.waitUntil(caches.open(staticCacheName).then(cache => {
+    console.log('caching shell assets')
+    cache.addAll(assets)
+  }))
+  
 });
 
 self.addEventListener('activate', evt => {
+  console.log('Service Worker has been activated')
+})
+
+self.addEventListener('fetch', evt => {
+  console.log('fetch evt', evt)
+
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
       return cacheRes || fetch(evt.request);
     })
   )
-});
+})
